ded9ef656243736e9fbd4ce1c71d3c3381d5831c
Centro Ricerca e Sviluppo Spazio Chirale <46569915+spaziochirale@users.noreply.github.com>
2019-02-27 20:46:01 +0100

    Add files via upload

---
+++ ProvaInterrupt-debunce.ino
@@ -0,0 +1,39 @@
+/***************************************************
+ *    ESEMPIO GESTIONE DEGLI INTERRUPT
+ *    Con la gestione del 'debounce'
+ *    
+ *    Author: Stefano Capezzone
+ *    Version: 1.0
+ *    Corso Professionale di arduino Avanzato
+ *    
+ */
+const byte BUTTON = 2; // Modificare con il PIN scelto anche in base al tipo di scheda
+volatile int stato = 1;
+volatile unsigned long tempo;
+volatile int contatore=0;
+
+
+void buttonPressed() {
+  if ((millis() - tempo) > 200) 
+    {stato = !stato;
+     contatore++;
+    }
+  if (stato == 1) digitalWrite(LED, HIGH);
+  else digitalWrite(LED, LOW);
+  tempo = millis();
+}
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(BUTTON, INPUT);
+  attachInterrupt(digitalPinToInterrupt(BUTTON), buttonPressed, FALLING);
+  tempo = millis();
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  
+  Serial.println(contatore);
+
+}
